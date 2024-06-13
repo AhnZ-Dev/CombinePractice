fafe66bb0974e8d86984d330f4729b49fdc9cca6
@AnJihun <jh.ahn@infovine.co.kr>
2024-06-13 16:44:40 +0900

    Combine PassThroughSubject이용한 playground코드 1. Publisher, Subscriber, Operator가 무엇인지 2. Combine Component 동작 원리 및 순서  맛보기

--- CombineBrainAdvent/CombineBrainAdvent/ViewController.swift
+++ CombineBrainAdvent/CombineBrainAdvent/ViewController.swift
@@ -4,16 +4,81 @@
 //
 //  Created by Jihoon on 6/12/24.
 //
-
+import Combine
 import UIKit
 
-class ViewController: UIViewController {
+/*
+ Publisher(날씨전송안테나)     ->      Time(온도 + 아이콘)   ->    Subscriber -> 라디오 + Operator(온도+아이콘에서 아이콘을 없애줘서 온도만 들어 갈 수 있게 해줌)
+ ** Publisher
+ 1. 어떻게 값과 에러를 정의하는가
+ 2. 값 타입
+ 3. 구독의 등록을 허락한다.
+ 
+ ** Subscriber
+ 1. 값, 완료여부를 수신
+ 2. 참조 타입
+ 
+ ** Operator
+ 1. Publisher를 채택
+ 2. 값 변경에 대한 행동 설명
+ 3. 업스트림이라고 부르는 퍼블리셔를 구독
+ 4. 다운스트림이라는 Subscriber에 데이터 전송
+ 5. 값 타입 (퍼블리셔와같음)
+ 
+ * protocol Publisher<Output, Failure> 보내는애니까 아웃풋 필요함
+ 
+ * Subscriber<Input, Failure> 받는애니까 Input이 필요함
+ sink(완료여부, 값), assign(지정장소에 넣어줌)
+ 
+ 
+ 
+ */
 
+enum WeatherError: Error {
+    case thingsJustHappen
+}
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        playgroundCombine() // playground형 연습 
+        
     }
-
-
+    
+    
+        
 }
 
+// playground형 Combine 연습
+extension ViewController {
+    func playgroundCombine(){
+        let weatherPublisher = PassthroughSubject<Int,Never>() // Publisher
+        
+        let subscriber = weatherPublisher // Subscriber
+            .filter {$0 > 25} // Operator
+            .sink { value in // 연결
+                print("A Summer day of \(value) ")
+            }
+        
+        // handleEvent로 Subscriber의 상태 및 호출 값을 받아 올 수 있다.
+        let anothersubscriber = weatherPublisher.handleEvents (receiveSubscription: { subscription in
+            print("NewSubscription \(subscription)") // 어떤 Subscriber가 구독되었는지!
+        }, receiveOutput: { output in // 어떤 것이 출력되는지
+            print("new Value: Output \(output)")
+        }, receiveCompletion: { err in // error 수신
+            print("potentialErr : \(err)")
+        }, receiveCancel: {
+            print("NewSubscription cancel")
+        }).sink{value in
+            print("Subscription : \(value)") // 들어온게 뭔지
+        }
+        
+        weatherPublisher.send(10)
+        weatherPublisher.send(20)
+        weatherPublisher.send(20)
+        weatherPublisher.send(30)
+        weatherPublisher.send(18) // send로 쏘면 이미 구독이 되어있기 떄문에 그냥 계속 타는거야
+    }
+}
