b186963e52c059eecf2d9e8be78ad50c8c278072
Ahnz <jihoon4680@gmail.com>
2024-06-18 22:00:24 +0900

    [작업중] - 코드 90% 완성 - 주석을 달지 않음

--- CombineKelvinFok/CombineKelvinFok/QuoteViewModel.swift
+++ CombineKelvinFok/CombineKelvinFok/QuoteViewModel.swift
@@ -22,12 +22,35 @@ class QuoteViewModel {
     }
     // Dependency Injection
     private let quetoServiceType: QuoteServiceType
+    private let output: PassthroughSubject<Output,Never> = .init()
+    private var cancellable = Set<AnyCancellable>()
+    // CurrentValue와 Passthrough의 차이점은 초기값(기존값)을 가지고 있냐의 차이다
+//    private let output2: CurrentValueSubject<Output,Never> = .init(.toggleButton(isEnable: true))
     
     init(quetoServiceType: QuoteServiceType = QuoteService()) {
         self.quetoServiceType = quetoServiceType
     }
     
-    func transforem(inpit : AnyPublisher<Input,Never) -> AnyPublisher<Output, Never> {
+    //Q: AnyPublisher???? 중요하지 않는 publisher를 구현
+    func transforem(input : AnyPublisher<Input,Never>) -> AnyPublisher<Output, Never> {
+        input.sink{ [weak self] event in
+            switch event {
+            case .viewDidApear, .tapRefreshButton:
+                self?.handleGetRandomQuote()
+            }
+        }.store(in: &cancellable)
+        
+        return output.eraseToAnyPublisher()
+    }
+    private func handleGetRandomQuote() {
+        quetoServiceType.getRandomQuote().sink { [weak self] completion in
+            if case .failure(let error) = completion {
+                self?.output.send(.fetchQuoteDidFail(error: error))
+            }
+        } receiveValue: { [weak self] quote in
+            self?.output.send(.fetchQuoteDidSuccess(quote: quote))
+        }.store(in: &cancellable)
+
         
     }
     
--- CombineKelvinFok/CombineKelvinFok/ViewController.swift
+++ CombineKelvinFok/CombineKelvinFok/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Jihoon on 6/16/24.
 //
-
+import Combine
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,13 +13,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var refreshButton: UIButton!
     
     private let vm = QuoteViewModel()
+    private let input: PassthroughSubject<QuoteViewModel.Input,Never> = .init()
+    private var cancellable = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        bind()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        input.send(.viewDidApear)
+    }
+    
+    private func bind(){
+        let output = vm.transforem(input: input.eraseToAnyPublisher())
+        
+        output
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] event in
+            switch event {
+            case .fetchQuoteDidSuccess(let quote):
+                self?.codeLabel.text = quote.content
+            case .fetchQuoteDidFail(let error):
+                self?.codeLabel.text = error.localizedDescription
+            case .toggleButton(let isEnable):
+                self?.refreshButton.isEnabled = isEnable
+            }
+        }.store(in: &cancellable)
+
     }
 
     @IBAction func tapRefreshButton(_ sender: UIButton) {
+        input.send(.tapRefreshButton)
     }
     
 }
